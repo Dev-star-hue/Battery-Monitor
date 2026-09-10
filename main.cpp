342307b8c5798d5d0771fceaeddf2438b8b4271f
DevMaverick <86577296+Dev-star-hue@users.noreply.github.com>
2026-09-10 16:09:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
 
 /**
- * @file main.cpp
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2022-11-30
- * 
- * @copyright Copyright (c) 2022
  * 
  * Where is a will there is a way!!
  * 
